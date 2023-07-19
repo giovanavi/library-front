3de29f44734921a21c664608215a4e5e3c4f0412
giovanavi <giovana.gvi02@gmail.com>
2023-07-19 12:55:00 -0300

    att

---
+++ src/app/views/livro/livro-main/livro-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-main',
+  templateUrl: './livro-main.component.html',
+  styleUrls: ['./livro-main.component.css']
+})
+export class LivroMainComponent {
+
+}
