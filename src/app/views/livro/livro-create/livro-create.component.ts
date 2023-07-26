f06762bcd4c0dcd79b2a74429c50b293579355db
giovanavi <giovana.gvi02@gmail.com>
2023-07-26 13:37:20 -0300

    adicionando validações básicas

--- src/app/views/livro/livro-create/livro-create.component.ts
+++ src/app/views/livro/livro-create/livro-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LivroService } from '../livro.service';
 import { Livro } from '../livro.model';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-livro-create',
@@ -20,15 +21,21 @@ export class LivroCreateComponent implements OnInit{
 
   constructor(private service:LivroService, private router: Router){}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
 
   create(): void{
     this.service.create(this.livro).subscribe(resposta => {
       this.router.navigate(['livros'])
       this.service.mensagem("Livro criado com sucesso !")
     }, err =>{
-      this.service.mensagem(err.error.message)
-    })
+      for (let i = 0; i < err.error.errors.length; i++) {
+        this.service.mensagem(err.error.errors[i].message)
+      }
+    }
+    )
   }
 
 }
+
